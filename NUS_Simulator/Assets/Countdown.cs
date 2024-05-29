2266209bcc8127b3ea2c2e43ae46fc492f14098f
Duong Ngoc Mai <lazycats121@gmail.com>
2024-05-29 14:53:51 +0700

    Add InGameScene
    A basic countdown text, placeholder objects for studying and distraction notifications

---
+++ NUS_Simulator/Assets/Countdown.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (remainingTime <= 0)
+        {
+            timerText.text = "00:00";
+        }
+    }
+}
